ee44a91209d243998d4ff26b3c7f939dc280f53d
coffeeman1a <128291592+coffeeman1a@users.noreply.github.com>
2023-06-06 11:04:41 +0300

    ball

--- MyFirstGame/Game1.cs
+++ MyFirstGame/Game1.cs
@@ -6,6 +6,10 @@ namespace MyFirstGame
 {
     public class Game1 : Game
     {
+        Texture2D ballTexture;
+        private Vector2 ballPos;
+        private float ballSpeed;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -19,7 +23,9 @@ namespace MyFirstGame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            ballPos = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                                  _graphics.PreferredBackBufferHeight / 2);
+            ballSpeed = 100f;
             base.Initialize();
         }
 
@@ -28,15 +34,46 @@ namespace MyFirstGame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            ballTexture = Content.Load<Texture2D>("ball");
+
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if(GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            var kstate = Keyboard.GetState();
 
+            if(kstate.IsKeyDown(Keys.Up))
+            {
+                ballPos.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if(kstate.IsKeyDown(Keys.Down))
+            {
+                ballPos.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if(kstate.IsKeyDown(Keys.Left))
+            {
+                ballPos.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if(kstate.IsKeyDown(Keys.Right))
+            {
+                ballPos.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if(ballPos.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+            {
+                ballPos.Y = _graphics.PreferredBackBufferHeight / 2 - ballTexture.Height;
+            }
+            else if(ballPos.Y < ballTexture.Height / 2)
+            {
+                ballPos.Y = ballTexture.Height / 2;
+            }
             base.Update(gameTime);
         }
 
@@ -45,6 +82,18 @@ namespace MyFirstGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(
+                ballTexture,
+                ballPos,
+                null,
+                Color.White,
+                0f,
+                new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
